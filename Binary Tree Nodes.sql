ec1c850135c4350b3cd8707cb9960dbf9d343f9f
Arkaan Ah'naf <ahnafkan@gmail.com>
2022-04-05 21:25:59 +0700

    Create Binary Tree Nodes.sql

---
+++ Binary Tree Nodes.sql
@@ -0,0 +1,14 @@
+/*
+Enter your query here.
+*/
+
+SELECT 
+    N,
+    CASE 
+        WHEN P IS NULL THEN "Root"
+        WHEN (SELECT COUNT(*) 
+              FROM BST 
+              WHERE P = A.N) > 0 THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM BST
