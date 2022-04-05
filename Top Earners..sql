4222fd96976e904391a5e4a295a30bce3b1080d0
Arkaan Ah'naf <ahnafkan@gmail.com>
2022-04-05 22:37:03 +0700

    Create Top Earners..sql

---
+++ Top Earners..sql
@@ -0,0 +1,9 @@
+/*
+Enter your query here.
+*/
+SELECT 
+        MAX(SALARY*MONTHS), 
+        COUNT(*)
+FROM EMPLOYEE
+WHERE (SALARY*MONTHS) = (SELECT MAX(SALARY*MONTHS)
+                         FROM EMPLOYEE);
