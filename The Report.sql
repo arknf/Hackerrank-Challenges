e23bc4c60bcc3c898e518708a4da8af037f7f382
Arkaan Ah'naf <ahnafkan@gmail.com>
2022-04-06 13:01:36 +0700

    Create The Report.sql

---
+++ The Report.sql
@@ -0,0 +1,10 @@
+/*
+Enter your query here.
+*/
+SELECT
+        IF(Grades.Grade >= 8, Students.Name, NULL),
+        Grades.Grade,
+        Students.Marks
+FROM Grades, Students
+WHERE Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark
+ORDER BY Grades.Grade DESC, Students.Name;
