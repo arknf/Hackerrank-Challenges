f79cec0e4539dea44a43c16a506f9713308cea5d
Arkaan Ah'naf <ahnafkan@gmail.com>
2022-04-07 21:42:37 +0700

    Create Department Top Three Salaries.sql

---
+++ Department Top Three Salaries.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT Department, Employee, Salary
+FROM (SELECT 
+        Department.name AS Department, 
+        Employee.name AS Employee, 
+        Employee.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY Department.name ORDER BY Salary DESC) AS RANKING
+     FROM Employee
+     INNER JOIN Department
+     ON Employee.departmentId = Department.id) AS temp
+WHERE RANKING <= 3;
